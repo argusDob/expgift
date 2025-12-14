bd5c93541c79040427bdbf24d15e0c1d522f3a3b
argusdob <giannisdimi@hotmail.com>
2025-12-14 15:20:08 +0100

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({ plugins:[vue()], server:{ port:5173 } })
+export default defineConfig({ 
+  plugins: [vue()], 
+  server: { port: 5173 },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/pixel.spec.ts']
+  }
+})
