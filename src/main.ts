0cbb51681c16b1b993f6d38586f73c0805dfaf1c
argusdob <giannisdimi@hotmail.com>
2025-12-14 15:21:35 +0100

    refactor: setup interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from './router'
 import App from './App.vue'
+import { setupAxiosInterceptors } from './services/axios-interceptors'
 import './styles.css'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+
+setupAxiosInterceptors() // Setup interceptors
+
 app.mount('#app')
